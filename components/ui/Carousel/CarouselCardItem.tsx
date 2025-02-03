3518d27ea9fff810be5142043fb90154193115e8
annikajnic <83368106+annikajnic@users.noreply.github.com>
2025-02-03 18:00:33 -0500

    in browser pop-up

--- components/ui/Carousel/CarouselCardItem.tsx
+++ components/ui/Carousel/CarouselCardItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, StyleSheet, Dimensions, Image } from "react-native";
+import { Dimensions, Image, ImageSourcePropType } from "react-native";
 import styled from "styled-components";
+import { ThemedView } from "../../ThemedView";
 
 export const SLIDER_WIDTH = Dimensions.get("window").width + 80;
 export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
@@ -26,52 +27,26 @@ const CarouselCardItem: React.FC<{ item: Item; index: number }> = ({
   index,
 }) => {
   const imageHeight = carouselOrentation(item.display);
-  console.log(imageHeight);
+
   return (
-    <CarouselView key={index}>
+    <CarouselView>
       <Image
-        source={{ uri: item.url }}
+        defaultSource={{ uri: item.url }}
         style={{
           width: ITEM_WIDTH,
           height: imageHeight,
+          aspectRatio: 1,
         }}
       />
     </CarouselView>
   );
 };
 
-const CarouselView = styled(View)`
-  backgroundcolor: white;
-  borderradius: 8;
+const CarouselView = styled(ThemedView)`
+  background-color: white;
+  border-radius: 8px;
   width: ${ITEM_WIDTH}px;
   elevation: 7;
 `;
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    borderRadius: 8,
-    width: ITEM_WIDTH,
-    paddingBottom: 40,
-    elevation: 7,
-  },
-  image: {
-    width: ITEM_WIDTH,
-    height: 250,
-  },
-  header: {
-    color: "#222",
-    fontSize: 28,
-    fontWeight: "bold",
-    paddingLeft: 20,
-    paddingTop: 20,
-  },
-  body: {
-    color: "#222",
-    fontSize: 18,
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
-});
 
 export default CarouselCardItem;
